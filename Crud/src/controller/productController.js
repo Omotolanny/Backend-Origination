f33f85f37649ab3432e0ed57473721c4b680eb62
Onifade Zeliahu <omotolanny>
2026-02-07 16:22:53 -0800

    Mini API project

--- Crud/src/controller/productController.js
+++ Crud/src/controller/productController.js
@@ -30,26 +30,26 @@ exports.createProduct = (req, res) => {
     res.status(201).json({ success: true, data: newProduct });
 };
 
-// 3. Logic to UPDATE a product
+
 exports.updateProduct = (req, res) => {
     const { id } = req.params;
     const { name, price } = req.body;
 
-    // Find the product by ID
+    
     const product = products.find(p => p.id === parseInt(id));
 
     if (!product) {
         return res.status(404).json({ success: false, message: "Product not found" });
     }
 
-    // Update the fields
+    
     if (name) product.name = name;
     if (price) product.price = price;
 
     res.status(200).json({ success: true, data: product });
 };
 
-// 4. Logic to DELETE a product
+
 exports.deleteProduct = (req, res) => {
     const { id } = req.params;
     const index = products.findIndex(p => p.id === parseInt(id));
